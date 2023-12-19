e5a5e892ed1e0d923e37ac3be5823d4c181a8082
tatyana <tilkka@mail.ru>
2023-12-19 11:23:19 +0300

    feat: matrix and simple-chain are done

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let result = 0;
+
+  for (let i = 0; i < matrix[0].length; i += 1) {
+    for (let row = 0; row < matrix.length; row += 1) {
+      if (matrix[row][i] === 0) {
+        break;
+      } else {
+        result += matrix[row][i];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
